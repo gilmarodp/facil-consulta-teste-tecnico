20f03d46a5238409d95d4f197ffe141e3e26a5f5
Gilmar Oliveira <gilmar.odp@gmail.com>
2025-01-30 21:24:33 -0300

    🗃️raw: adiciona novos usuarios para login no seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@email.com',
+        ]);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Christian Ramires',
+            'email' => 'christian.ramires@example.com',
         ]);
     }
 }
